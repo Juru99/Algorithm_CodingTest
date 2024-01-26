76f88292ecc826b30973b5b6faac17d050cbc956
Juru99 <96530621+Juru99@users.noreply.github.com>
2024-01-26 19:00:56 +0900

    [Bronze II] Title: Sum Game, Time: 48 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/14686. Sum Game/Sum Game.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int sumGame(int gameCount) {
+	int sameCount = 0;
+	vector<int> swifts(gameCount + 1);
+	vector<int> semaphores(gameCount + 1);
+
+	for (int i = 1; i <= gameCount; i++) {
+		cin >> swifts[i];
+		swifts[i] = swifts[i - 1] + swifts[i];
+	}
+	for (int i = 1; i <= gameCount; i++) {
+		cin >> semaphores[i];
+		semaphores[i] = semaphores[i - 1] + semaphores[i];
+	}
+	for (int i = 1; i <= gameCount; i++) {
+		if (swifts[i] == semaphores[i]) {
+			sameCount = i;
+		}
+	}
+
+	return sameCount;
+}
+
+int main() {
+	int gameCount = 0;
+	cin >> gameCount;
+
+	cout << sumGame(gameCount);
+
+	return 0;
+}
